701e6d27143b8e3d212b5a6e44e4f105885dd2ce
Skelesheit <Skelesheit@gmail.com>
2025-02-14 19:30:24 +0300

    сделана нормальная шапка

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,18 @@
-import { Routes } from '@angular/router';
-import {AppComponent} from './app.component';
+import {provideRouter, Routes, withDebugTracing} from '@angular/router';
 import {CameraRecordsComponent} from './Pages/camera-records/camera-records.component';
 import {ScheduleComponent} from './Pages/schedule/schedule.component';
 import {AttendanceGraphsComponent} from './Pages/attendance-graphs/attendance-graphs.component';
+import {ApplicationConfig} from '@angular/core';
 
 export const routes: Routes = [
 
-  { path: '', redirectTo: '/camera-records', pathMatch: 'full' }, // Стартовый маршрут
-  { path: 'camera-records', component: CameraRecordsComponent },
-  { path: 'schedule', component: ScheduleComponent },
-  { path: 'attendance-graphs', component: AttendanceGraphsComponent }
+  {path: '', redirectTo: '/camera-records', pathMatch: 'full'}, // Стартовый маршрут
+  {path: 'camera-records', component: CameraRecordsComponent, pathMatch: 'full'},
+  {path: 'schedule', component: ScheduleComponent, pathMatch: 'full'},
+  {path: 'attendance-graphs', component: AttendanceGraphsComponent}
 
 ];
+
+export const appConfig = {
+  providers: [provideRouter(routes)]
+};
--- src/app/components/root-header/root-header.component.ts
+++ src/app/components/root-header/root-header.component.ts
@@ -1,13 +1,26 @@
 import {Component, OnInit} from '@angular/core';
+import {Router, RouterLinkActive} from '@angular/router';
+import {NgClass, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-root-header',
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    NgClass,
+    RouterLinkActive
+  ],
   templateUrl: './root-header.component.html',
   standalone: true,
   styleUrl: './root-header.component.css'
 })
 export class RootHeaderComponent implements OnInit {
+
+  constructor(private router: Router) {
+  }
+
+  isActive(route: string): boolean {
+    return this.router.url === route;
+  }
 
   ngOnInit(): void {
     // Проверяем сохраненную тему при загрузке страницы
