54c5ca769c78f538cf9f650d5f4054b703024335
Skelesheit <Skelesheit@gmail.com>
2025-02-28 01:04:37 +0300

    сделана нормальная шапка

---
+++ src/app/components/schedule-selector/schedule-selector.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule-selector',
+  imports: [],
+  templateUrl: './schedule-selector.component.html',
+  standalone: true,
+  styleUrl: './schedule-selector.component.css'
+})
+export class ScheduleSelectorComponent {
+
+}
